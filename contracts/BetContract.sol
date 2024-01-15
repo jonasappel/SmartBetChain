9e6efa69d7f734ef594c6de63915f1bc317a07e0
jonasappel <jonas.appel95@gmail.com>
2024-01-15 11:48:23 +0100

    added isOver-attribute to BetContract

--- contracts/BetContract.sol
+++ contracts/BetContract.sol
@@ -1,29 +1,32 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 //atm supports only 2 bettors betting against each other for the same amount of money (no sanity checks)
 contract BetContract {
     address public owner;
-    uint allBets = 0; //store all bets combined 
-
-    //represents the sports match bet on
-    struct Game {   
-        string name;    // e.g. "FCB - BVB"
-        uint8 winner;    // 0 = Pending, 1 = Hometeam win, 2 = Guestteam win, 3 = Draw
-    }
-
-    //TODO: make this an array to store more games
-    Game public game;   //represents the game
+    uint maxBetCount = 100;
+    uint allBets = 0; //store all bet amounts combined -> TODO: change that to work better
 
     //represents 1 bet
     struct Bet {
         address user;
-        // Game game;
+        Game game;
         uint8 winPrediction;
         uint amount;
     }
+    
+    //represents the sports match bet on
+    struct Game {   
+        string name;    // e.g. "FCB - BVB"
+        uint8 winner;    // 0 = Draw, 1 = Hometeam win, 2 = Guestteam win
+        bool isOver;
+        // TODO: Bet[] bets; -> Fertig/Schütz fragen
+    }
 
-    Bet[] public bets;     //represents all bets
+    //TODO: make this an array to store more games
+    Game public game;   //represents the game
+    Bet[] bets;     //represents all bets
 
     constructor() {
         owner = msg.sender;
@@ -36,32 +39,32 @@ contract BetContract {
 
     //add a game using a name (only by owner)
     function addGame(string memory _name) public onlyOwner {
-        game = Game(_name, 0);
+        game = Game(_name, 0, false);   //game = Game(_name, 0, false, new Bet[](100));
     }
 
     //add winner to existing game (only by owner)
     function addGameWinner(uint8 _winner) public onlyOwner {
         game.winner = _winner;
+        game.isOver = true;
     }
 
-    //make this functional ????
     function removeGame() public onlyOwner {
         delete game;
     }
 
     //Todo: Make this function be only usable when not owner of contract
     function addBet(uint8 _winPrediction) public payable {
-        require(game.winner == 0, "Winner is already stated");
-        //sanity checks (bettor already bet?, has different WinCondition?)
+        require(game.isOver == false, "Game is over - no bets allowed");
+        //Todo: sanity checks (bettor already bet?, has different WinCondition?)
 
         allBets += msg.value;
 
-        bets.push(Bet(msg.sender, _winPrediction, msg.value));
+        bets.push(Bet(msg.sender, game, _winPrediction, msg.value));
     }
 
     //Todo: Pay the SC Value to the Winner
-    function declareWinner() public payable onlyOwner {
-        require(game.winner != 0, "Winner is not yet stated");
+    function declareBetWinner() public payable onlyOwner {
+        require(game.isOver == true, "Game is not yet over");
 
         // transfer money to the 1 winner of the bet
         for (uint i = 0; i < bets.length; i++) {
@@ -70,6 +73,8 @@ contract BetContract {
                 payable(bet.user).transfer(allBets);
             }
         }
+
+        //handle what happens if nobody wins
     }
 
 }

046f4fc4be80f9391123d2b580ce522226743196
jonasappel <jonas.appel95@gmail.com>
2024-01-16 19:46:34 +0100

    improved BetContract in various ways

--- contracts/BetContract.sol
+++ contracts/BetContract.sol
@@ -5,28 +5,31 @@ pragma solidity ^0.8.0;
 //atm supports only 2 bettors betting against each other for the same amount of money (no sanity checks)
 contract BetContract {
     address public owner;
-    uint maxBetCount = 100;
-    uint allBets = 0; //store all bet amounts combined -> TODO: change that to work better
+    Game[] public games;   //represents the games
+    //Bet[] bets;     //represents all bets
 
     //represents 1 bet
     struct Bet {
         address user;
-        Game game;
+        uint gameId;
         uint8 winPrediction;
-        uint amount;
     }
     
     //represents the sports match bet on
-    struct Game {   
+    struct Game {
+        uint id;   
         string name;    // e.g. "FCB - BVB"
-        uint8 winner;    // 0 = Draw, 1 = Hometeam win, 2 = Guestteam win
+        uint8 winner;    // 0 = Initial, 1 = Hometeam win, 2 = Guestteam win, 3 = Draw
         bool isOver;
-        // TODO: Bet[] bets; -> Fertig/Schütz fragen
+        uint betAmount;         //?Wetteinsatz festlegen
+        uint counterTeamOne;    //counter for bets placed on team 1
+        uint counterTeamTwo;    //counter for bets placed on team 2
+        uint counterDraw;       //counter for bets placed on draw
+        mapping(address => Bet) bets;     // Use a mapping for bets with userAddress => Bet
+        uint betCount; // Track the number of bets for iteration
     }
 
-    //TODO: make this an array to store more games
-    Game public game;   //represents the game
-    Bet[] bets;     //represents all bets
+    //Oracles durchsuchen / API zum Ergebnisse abfragen (-> ChainLink DataFeed) => Niklas/Kujtim
 
     constructor() {
         owner = msg.sender;
@@ -37,44 +40,62 @@ contract BetContract {
         _;
     }
 
-    //add a game using a name (only by owner)
-    function addGame(string memory _name) public onlyOwner {
-        game = Game(_name, 0, false);   //game = Game(_name, 0, false, new Bet[](100));
+    //add a new game using a name (only by owner)
+    function addGame(string memory _name, uint _betAmount) public onlyOwner {
+        uint gameId = games.length;
+        Game storage newGame = games.push();
+        newGame.id = gameId;
+        newGame.name = _name;
+        newGame.betAmount = _betAmount;
     }
 
     //add winner to existing game (only by owner)
-    function addGameWinner(uint8 _winner) public onlyOwner {
-        game.winner = _winner;
-        game.isOver = true;
+    function addGameWinner(uint8 _gameId, uint8 _winner) public onlyOwner {
+        games[_gameId].winner = _winner;
+        games[_gameId].isOver = true;
     }
 
-    function removeGame() public onlyOwner {
-        delete game;
-    }
+    //remove game (only owner) -> no good to delete items from map as keys are incremented
+    //function removeGame(uint8 _gameId) public onlyOwner {
+    //    delete games[_gameId];
+    //}
 
     //Todo: Make this function be only usable when not owner of contract
-    function addBet(uint8 _winPrediction) public payable {
-        require(game.isOver == false, "Game is over - no bets allowed");
-        //Todo: sanity checks (bettor already bet?, has different WinCondition?)
+    function addBet(uint8 _gameId, uint8 _winPrediction) public payable {
+        require(!games[_gameId].isOver, "Game is over - no bets allowed anymore");
+        require(games[_gameId].bets[msg.sender].winPrediction == 0, "You already bet - no bets allowed anymore");
+        require(msg.value == games[_gameId].betAmount, "Bet does not equal the specified bet amount for this game");
 
-        allBets += msg.value;
-
-        bets.push(Bet(msg.sender, game, _winPrediction, msg.value));
+        // möglicherweise prüfen, ob Spieler = echter Spieler oder SC => nur echte Spieler 
+        
+        //add the bet to the game
+        games[_gameId].bets[msg.sender] = Bet(msg.sender, _gameId, _winPrediction);
+        games[_gameId].betCount++;
+        if (_winPrediction == 1)  games[_gameId].counterTeamOne++;
+        if (_winPrediction == 2)  games[_gameId].counterTeamTwo++;
+        if (_winPrediction == 3)  games[_gameId].counterDraw++;
     }
 
-    //Todo: Pay the SC Value to the Winner
-    function declareBetWinner() public payable onlyOwner {
-        require(game.isOver == true, "Game is not yet over");
+    function collectWinnings(uint _gameId) public {
+        require(games[_gameId].isOver, "Game is not yet over - Bets are still pending");
+        require(games[_gameId].bets[msg.sender].winPrediction == games[_gameId].winner, "Sorry - you lost the bet");
 
-        // transfer money to the 1 winner of the bet
-        for (uint i = 0; i < bets.length; i++) {
-            Bet memory bet = bets[i];
-            if (bet.winPrediction == game.winner) {
-                payable(bet.user).transfer(allBets);
-            }
+        uint winAmount;
+
+        if(games[_gameId].winner == 1) {
+            winAmount = (games[_gameId].betCount*games[_gameId].betAmount)/games[_gameId].counterTeamOne;
+        }
+        else if(games[_gameId].winner == 2) {
+            winAmount = (games[_gameId].betCount*games[_gameId].betAmount)/games[_gameId].counterTeamTwo;
+        }
+        else if(games[_gameId].winner == 3) {
+            winAmount = (games[_gameId].betCount*games[_gameId].betAmount)/games[_gameId].counterDraw;
         }
 
-        //handle what happens if nobody wins
+        //transfer money to the sender
+        payable(msg.sender).transfer(winAmount);
+
+        //what happens with the money on the contract if nobody wins or collects winnings ?
     }
 
 }
